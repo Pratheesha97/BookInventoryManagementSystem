be226d9fe82135a80410c9e0ac97c0cb7f4b1720
Samadhi Pratheesha <55917205+Pratheesha97@users.noreply.github.com>
2024-01-02 23:18:10 +0530

    Update Add.jsx

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -21,7 +21,7 @@ const Add = () => {
   const handleClick = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:8800/books", book);
+      await axios.post("http://16.171.14.47:8800/books", book);
       navigate("/");
     } catch (err) {
       console.log(err);
