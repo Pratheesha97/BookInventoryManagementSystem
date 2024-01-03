80075dcd99375dd415e33470a2cd7174cc0bed8e
Samadhi Pratheesha <55917205+Pratheesha97@users.noreply.github.com>
2024-01-03 14:37:27 +0530

    Update before_script.sh

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -3,9 +3,9 @@
 set -e
 
 cd "$(dirname "$0")/../client"
-npm install
+npm install -y
 npm run build
 cd ..
-npm i concurrently
-npm install -g nodemon
-npm install
+npm i -y concurrently
+npm install -g nodemon -y
+npm install -y
